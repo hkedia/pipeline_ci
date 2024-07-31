a1e295d65a5906c8e831e4137392a7612f9e24e3
Harsh Kedia <hkedia@hey.com>
2024-07-31 08:00:00 +0530

    add dialyxir as dependency

--- mix.exs
+++ mix.exs
@@ -68,7 +68,8 @@ defmodule PipelineCi.MixProject do
       {:bandit, "~> 1.2"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
-      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
